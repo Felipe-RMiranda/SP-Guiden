38767cdc9bf50997ca330712ac1118980bf0f360
Fellpe-R <fellpe.ramos@outlook.com>
2024-07-06 21:55:57 -0300

    Initial commit

---
+++ app/src/main/java/com/mirandar/spguiden/Started_page.kt
@@ -0,0 +1,32 @@
+package com.mirandar.spguiden
+
+import android.os.Bundle
+import android.widget.Button
+import android.widget.Toast
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class Started_page : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.started_page)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+
+        }
+        val started: Button = findViewById(R.id.btnStart)
+
+        started.setOnClickListener{
+            showMessage("More soon")
+        }
+    }
+
+    fun showMessage(message:String) {
+        Toast.makeText(this, message, Toast.LENGTH_LONG).show()
+    }
+}
